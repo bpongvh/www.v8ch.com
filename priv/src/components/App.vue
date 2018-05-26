61d2ec8b9434cf3fb44d25413462b74d13f4190c
Robert Pratt <bpong@v8ch.com>
2018-05-26 17:24:11 +0700

    Moved

---
+++ priv/src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <V8chLogo />
+        <p>Hello, world!</p>
+    </div>
+</template>
+
+<script lang="babel">
+  import V8chLogo from './icons/V8chLogo.vue';
+
+  export default {
+    components: { V8chLogo },
+  }
+</script>
+
+<style lang="scss">
+</style>
