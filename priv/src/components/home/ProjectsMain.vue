993fac6663ff6697833b52e38cd5041697132e53
Robert Pratt <bpong@v8ch.com>
2018-05-27 16:51:12 +0700

    Add main--2col, featured-link--margin-bottom

--- priv/src/components/home/ProjectsMain.vue
+++ priv/src/components/home/ProjectsMain.vue
@@ -5,26 +5,26 @@
         <h3>Projects</h3>
       </div>
     </div>
-    <div class="main main--2col">
-      <div class="featured-link featured-link--light">
+    <div class="main__2col">
+      <div class="featured-link featured-link--light featured-link--margin-bottom">
         <h5>
           <a href="https://github.com/V8CH">Code Repositories</a>
         </h5>
         <p>V8CH at GitHub</p>
       </div>
-      <div class="featured-link featured-link--light">
+      <div class="featured-link featured-link--light featured-link--margin-bottom">
         <h5>
           <a href="https://www.departmentware.com">Departmentware</a>
         </h5>
         <p>SAAS platform for managing documentation of field training for new police officers</p>
       </div>
-      <div class="featured-link featured-link--light">
+      <div class="featured-link featured-link--light featured-link--margin-bottom">
         <h5>
           <a href="https://combine.team/">Combine</a>
         </h5>
-        <p>Team organization architexture built with Laravel, React and VueJS (demo on request)</p>
+        <p>Team organization architecture built with Laravel, React and VueJS (demo on request)</p>
       </div>
-      <div class="featured-link featured-link--light">
+      <div class="featured-link featured-link--light featured-link--margin-bottom">
         <h5>
           <a href="http://www.jeromegoldschmidt.com">jeromegoldschmidt.com</a>
         </h5>
