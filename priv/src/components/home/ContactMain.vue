2fe0f62b6eb66eee96e6737d80540846f1fdaeb8
Robert Pratt <bpong@v8ch.com>
2018-05-29 09:48:25 +0700

    WIP: Build-out

--- priv/src/components/home/ContactMain.vue
+++ priv/src/components/home/ContactMain.vue
@@ -6,7 +6,10 @@
       </div>
     </div>
     <div class="main_form-container main__container--margin-bottom">
-      <form novalidate>
+      <form
+        novalidate
+        @submit.prevent="onSubmit"
+      >
         <div class="form-group">
           <label
             class="v8ch-dark-green"
@@ -14,6 +17,7 @@
           >Email address</label>
           <input
             id="email"
+            v-model="email"
             class="form-control"
             placeholder="Email address"
             type="email"
@@ -29,6 +33,7 @@
           >Message</label>
           <textarea
             id="message"
+            v-model="message"
             class="form-control"
             rows="5"
             placeholder="Message"
@@ -46,26 +51,69 @@
       </form>
     </div>
     <div class="main__container main__container--space-between">
-      <github-logo size="small" />
-      <v8ch-logotype
-        size="small"
-        @click="viewAtGithub"
-      />
+      <div class="social-icons">
+        <github-logo
+          size="small"
+          @click="viewAtGithub"
+        />
+        <twitter-logo
+          size="small"
+          @click="viewAtTwitter"
+        />
+      </div>
+      <v8ch-logotype size="small" />
     </div>
   </div>
 </template>
 
 <script type="text/babel">
 import GithubLogo from '../icons/GithubLogo.vue';
+import TwitterLogo from '../icons/TwitterLogo.vue';
 import V8chLogotype from '../icons/V8chLogotype.vue';
+import ApiService from '../../services/api-service';
 
 export default {
   components: {
     GithubLogo,
+    TwitterLogo,
     V8chLogotype,
+  },
+
+  // ----------------------
+  // Data
+  // ----------------------
+
+  data() {
+    return {
+      email: '',
+      message: '',
+    };
+  },
+
+  // ----------------------
+  // Methods
+  // ----------------------
+
+  methods: {
+    onSubmit() {
+      const post = { email: this.email, message: this.message };
+      console.log(`[ContactMain] onSubmit() post: ${JSON.stringify(post)}`);
+      ApiService.post('/api/v1/contact', post);
+    },
+    viewAtGithub() {
+      window.location.href = 'https://github.com/V8CH';
+    },
+    viewAtTwitter() {
+      window.location.href = 'https://twitter.com/bponghneng';
+    },
   },
 };
 </script>
 
 <style lang="scss">
+.social-icons {
+  & > :not(:last-child) {
+    padding: 0 1em 0 0;
+  }
+}
 </style>
