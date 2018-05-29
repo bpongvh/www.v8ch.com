be78626e14ca5dd3f819282d489512f174082a24
Robert Pratt <bpong@v8ch.com>
2018-05-29 09:48:46 +0700

    Bugfix

--- test/v8ch_web/controllers/page_controller_test.exs
+++ test/v8ch_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule V8chWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "<div id=\"v8ch\""
   end
 end
