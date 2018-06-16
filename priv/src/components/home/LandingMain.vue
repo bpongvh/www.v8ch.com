9894f6fdaa3a2a3654f64fb0468829762488e977
Robert Pratt <bpong@v8ch.com>
2018-06-16 09:20:05 +0700

    Move LandingMain to functional

--- priv/src/components/home/LandingMain.vue
+++ priv/src/components/home/LandingMain.vue
@@ -1,4 +1,4 @@
-<template functional>
+<template>
   <div class="main main--dark-background main--hero">
     <div class="main__container">
       <div class="dual-header dual-header--light">
@@ -17,3 +17,37 @@
     </div>
   </div>
 </template>
+
+<script type="text/babel">
+import V8chLogo from '../icons/V8chLogo.vue';
+
+export default {
+  components: {
+    V8chLogo,
+  },
+
+  // ----------------------
+  // Data
+  // ----------------------
+
+  data() {
+    return { logoSize: 'small' };
+  },
+
+  // ----------------------
+  // Lifecycle
+  // ----------------------
+
+  mounted() {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      this.logoSize = 'medium';
+    }
+    if (window.matchMedia('(min-width: 992px)').matches) {
+      this.logoSize = 'large';
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
