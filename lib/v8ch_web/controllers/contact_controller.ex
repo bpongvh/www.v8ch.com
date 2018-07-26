f5b4215c6cda8d24933f533b1eae8b311967f11c
Robert Pratt <bpong@v8ch.com>
2018-07-26 07:27:55 +0700

    Add email

--- lib/v8ch_web/controllers/contact_controller.ex
+++ lib/v8ch_web/controllers/contact_controller.ex
@@ -1,13 +1,24 @@
 defmodule V8chWeb.ContactController do
+  import Bamboo.Email
   use V8chWeb, :controller
 
   alias V8ch.Contacts
   alias V8chWeb.ContactView
   alias V8chWeb.ChangesetView
+  alias V8ch.Mailer
 
   def create(conn, %{"email" => email, "message" => message}) do
     case Contacts.create_contact(%{email: email, message: message}) do
       {:ok, contact} ->
+        message =
+          new_email
+          |> to("bpong@v8ch.com")
+          |> from(email)
+          |> subject("v8ch.com Contact Message from #{email}")
+          |> text_body(message)
+
+          message
+          |> Mailer.deliver_later
         conn
         |> render(ContactView, "contact.json", %{contact: contact})
 
