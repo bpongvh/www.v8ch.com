a2beb715df8f326ac2bc2da6335a87e0b50a8ea9
Robert Pratt <bpong@v8ch.com>
2018-08-03 13:19:35 +0700

    Add mutation create_featured_link; update middleware

--- lib/v8ch_web/graphql/schema.ex
+++ lib/v8ch_web/graphql/schema.ex
@@ -1,43 +1,49 @@
 defmodule V8chWeb.GraphQlSchema do
-    use Absinthe.Schema
-  
-    alias V8chWeb.GraphQlMiddleware.CheckAuthentication, as: CheckAuthenticationMiddleware
-  
-    # ----------------------
-    # Import fields
-    # ----------------------
-  
-    import_types(V8chWeb.GraphQlFields.FeaturedLinks)
-  
-    # ----------------------
-    # Import Types
-    # ----------------------
-  
-    import_types(V8chWeb.GraphQlTypes.FeaturedLink)
-  
-    query do
-      import_fields :featured_links_queries
-    end
-    def middleware(middleware, _field, _object) do
-      [CheckAuthenticationMiddleware] ++ middleware
-      # middleware
-    end
-  
-    # mutation do
-    #   field :login, :user_context do
-    #     arg(:input, non_null(:cognito_access_token_input))
-    #     resolve(&AuthResolver.verify_and_sign_in/3)
-    #   end
-    # end
-  
-    # scalar :json do
-    #   parse fn input ->
-    #     case Poison.decode(input.value) do
-    #       {:ok, result} -> result
-    #       _ -> :error
-    #     end
-    #   end
-  
-    #   serialize &Poison.encode!/1
-    # end
+  use Absinthe.Schema
+
+  alias V8chWeb.GraphQlMiddleware.CheckAuthentication, as: CheckAuthenticationMiddleware
+  alias V8chWeb.GraphQlMiddleware.HandleChangesetErrors, as: HandleChangesetErrorsMiddleware
+  alias V8chWeb.GraphQlResolvers.FeaturedLinks, as: FeaturedLinksResolver
+
+  # ----------------------
+  # Import fields
+  # ----------------------
+
+  import_types(V8chWeb.GraphQlFields.FeaturedLinks)
+
+  # ----------------------
+  # Import Types
+  # ----------------------
+
+  import_types(V8chWeb.GraphQlTypes.FeaturedLink)
+
+  query do
+    import_fields(:featured_links_queries)
   end
+
+  mutation do
+    field :create_featured_link, :featured_link_mutation_result do
+      arg(:data, non_null(:featured_link_input))
+      middleware CheckAuthenticationMiddleware
+      resolve(&FeaturedLinksResolver.create_featured_link/3)
+      middleware HandleChangesetErrorsMiddleware
+    end
+  end
+
+  @desc "An error result when persisting input"
+  object :input_error do
+    field(:key, non_null(:string))
+    field(:message, non_null(:string))
+  end
+
+  # scalar :json do
+  #   parse fn input ->
+  #     case Poison.decode(input.value) do
+  #       {:ok, result} -> result
+  #       _ -> :error
+  #     end
+  #   end
+
+  #   serialize &Poison.encode!/1
+  # end
+end
