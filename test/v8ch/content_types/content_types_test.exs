4efd756f4ed74aef74be563b6c96c4a7eb0b445f
Robert Pratt <bpong@v8ch.com>
2018-08-04 19:46:31 +0700

    Add Post tests

--- test/v8ch/content_types/content_types_test.exs
+++ test/v8ch/content_types/content_types_test.exs
@@ -66,4 +66,66 @@ defmodule V8ch.ContentTypesTest do
       assert %Ecto.Changeset{} = ContentTypes.change_featured_link(featured_link)
     end
   end
+
+  describe "POSTS CONTEXT MODULE" do
+    alias V8ch.ContentTypes.Post
+
+    @valid_attrs %{content: "some content", title: "some title"}
+    @update_attrs %{content: "some updated content", title: "some updated title"}
+    @invalid_attrs %{content: nil, title: nil}
+
+    def post_fixture(attrs \\ %{}) do
+      {:ok, post} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> ContentTypes.create_post()
+
+      post
+    end
+
+    test "list_posts/0 returns all posts" do
+      post = post_fixture()
+      assert ContentTypes.list_posts() == [post]
+    end
+
+    test "get_post!/1 returns the post with given id" do
+      post = post_fixture()
+      assert ContentTypes.get_post!(post.id) == post
+    end
+
+    test "create_post/1 with valid data creates a post" do
+      assert {:ok, %Post{} = post} = ContentTypes.create_post(@valid_attrs)
+      assert post.content == "some content"
+      assert post.title == "some title"
+    end
+
+    test "create_post/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = ContentTypes.create_post(@invalid_attrs)
+    end
+
+    test "update_post/2 with valid data updates the post" do
+      post = post_fixture()
+      assert {:ok, post} = ContentTypes.update_post(post, @update_attrs)
+      assert %Post{} = post
+      assert post.content == "some updated content"
+      assert post.title == "some updated title"
+    end
+
+    test "update_post/2 with invalid data returns error changeset" do
+      post = post_fixture()
+      assert {:error, %Ecto.Changeset{}} = ContentTypes.update_post(post, @invalid_attrs)
+      assert post == ContentTypes.get_post!(post.id)
+    end
+
+    test "delete_post/1 deletes the post" do
+      post = post_fixture()
+      assert {:ok, %Post{}} = ContentTypes.delete_post(post)
+      assert_raise Ecto.NoResultsError, fn -> ContentTypes.get_post!(post.id) end
+    end
+
+    test "change_post/1 returns a post changeset" do
+      post = post_fixture()
+      assert %Ecto.Changeset{} = ContentTypes.change_post(post)
+    end
+  end
 end
