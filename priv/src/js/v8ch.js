6c2b3c74a72d526de6ddb82a985d8989d0cc66e4
Robert Pratt <bpong@v8ch.com>
2018-08-04 18:34:35 +0700

    Integrate VueJS and Apollo

--- priv/src/js/v8ch.js
+++ priv/src/js/v8ch.js
@@ -1,9 +1,23 @@
-import Vue from 'vue';
-import App from './components/App.vue';
-// import '../scss/style.scss';
+import VueApollo from "vue-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import Vue from "vue";
+import App from "./components/App.vue";
 
-console.log(`[v8ch app] in s true: ${JSON.stringify(true)}`);
+// Apollo config
+const httpLink = new HttpLink({
+  uri: process.env.GRAPHQL_HOST_URI
+});
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: process.env.NODE_ENV !== "production"
+});
+const apolloProvider = new VueApollo({ defaultClient: apolloClient });
+Vue.use(VueApollo);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#v8ch');
+  provide: apolloProvider.provide(),
+  render: h => h(App)
+}).$mount("#v8ch");
