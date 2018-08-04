9bd26612781b1ce9a8e5dbde09258c6fc8c3adf0
Robert Pratt <bpong@v8ch.com>
2018-08-04 14:33:24 +0700

    Check environment; stub user for test

--- lib/v8ch_web/plugs/graphql_context.ex
+++ lib/v8ch_web/plugs/graphql_context.ex
@@ -1,34 +1,46 @@
 defmodule V8chWeb.Plugs.GraphQlContext do
-    import Plug.Conn
-    require Logger
-  
-    @behaviour Plug
-  
-    @contexts_module Application.get_env(:v8ch, :contexts_module)
-  
-    def init(_), do: :ok
-  
-    def call(conn, _) do
-      with {:ok, jwt} <- get_token(conn),
-           {:ok, verified_jwt} <- @contexts_module.verify_jwt(jwt),
-           {:ok, user} <- @contexts_module.get_user_from_jwt(verified_jwt) do
-        Absinthe.Plug.put_options(conn, context: %{authenticated: user})
-      else
-        {:error, error} ->
-          Logger.error("[V8ch.Plugs.GraphQlContext] Error fetching context: #{error}")
-          Absinthe.Plug.put_options(conn, context: %{error: error})
-  
-        unknown ->
-          Logger.error("[V8ch.Plugs.GraphQlContext] Unknown error")
-          Absinthe.Plug.put_options(conn, context: %{error: unknown})
-      end
-    end
-  
-    defp get_token(conn) do
-      with ["Bearer " <> jwt] <- get_req_header(conn, "authorization") do
-        {:ok, jwt}
-      else
-        _ -> {:error, "TOKEN_NOT_FOUND"}
-      end
+  import Plug.Conn
+  require Logger
+
+  @behaviour Plug
+
+  @contexts_module Application.get_env(:v8ch, :contexts_module)
+
+  def init(_), do: :ok
+
+  def call(conn, _) do
+    with :ok <- check_environment(),
+         {:ok, jwt} <- get_token(conn),
+         {:ok, verified_jwt} <- @contexts_module.verify_jwt(jwt),
+         {:ok, user} <- @contexts_module.get_user_from_jwt(verified_jwt) do
+      Absinthe.Plug.put_options(conn, context: %{authenticated: user})
+    else
+      {:error, error} ->
+        Logger.error("[V8ch.Plugs.GraphQlContext] Error fetching context: #{error}")
+        Absinthe.Plug.put_options(conn, context: %{error: error})
+
+      :test ->
+        Absinthe.Plug.put_options(conn, context: %{authenticated: %{username: "test"}})
+
+      unknown ->
+        Logger.error("[V8ch.Plugs.GraphQlContext] Unknown error")
+        Absinthe.Plug.put_options(conn, context: %{error: unknown})
     end
   end
+
+  defp check_environment() do
+    case Application.get_env(:v8ch, :env) do
+      "test" -> :test
+      _ -> :ok
+    end
+  end
+
+  defp get_token(conn) do
+    with ["Bearer " <> jwt] <- get_req_header(conn, "authorization") do
+      {:ok, jwt}
+    else
+      _ -> {:error, "TOKEN_NOT_FOUND"}
+    end
+  end
+end
+
