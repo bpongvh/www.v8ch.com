48d29604bde289bc74113d3e10edef59f701c454
Robert Pratt <bpong@v8ch.com>
2018-08-05 07:50:15 +0700

    Add @moduletag; add test for not found

--- test/v8ch_web/graphql/mutation/update_featured_link_test.exs
+++ test/v8ch_web/graphql/mutation/update_featured_link_test.exs
@@ -1,6 +1,8 @@
 defmodule V8chWeb.GraphQl.Mutation.UpdateFeaturedLinkTest do
   use V8chWeb.ConnCase, async: true
 
+  @moduletag :update_featured_link_mutation
+
   alias V8ch.ContentTypes
 
   describe "UPDATE FEATURED LINKS: " do
@@ -47,6 +49,27 @@ defmodule V8chWeb.GraphQl.Mutation.UpdateFeaturedLinkTest do
 
     setup do
       {:ok, [featured_link: featured_link_fixture(@valid_attrs)]}
+    end
+
+    test "updating a featured_link returns not found with nonexistent id", _ do
+      conn = build_conn()
+
+      conn =
+        post(conn, "/graphql/v1",
+          query: @query,
+          variables: %{"featuredLink" => Enum.into(%{id: 7}, @update_attrs)}
+        )
+
+      assert json_response(conn, 200) == %{
+               "data" => %{"updateFeaturedLink" => nil},
+               "errors" => [
+                 %{
+                   "locations" => [%{"column" => 0, "line" => 2}],
+                   "message" => "FEATURED_LINK_NOT_FOUND",
+                   "path" => ["updateFeaturedLink"]
+                 }
+               ]
+             }
     end
 
     test "updating a featured_link validates data", %{featured_link: featured_link} do
