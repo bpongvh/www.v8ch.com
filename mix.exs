bf5b22eaffaeaa1c76fbed69077b2d6f0d00df68
Robert Pratt <bpong@v8ch.com>
2018-08-05 07:59:30 +0700

    Bump versions

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule V8ch.Mixfile do
   def project do
     [
       app: :v8ch,
-      version: "0.0.3",
-      elixir: "~> 1.4",
+      version: "0.1.0",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
