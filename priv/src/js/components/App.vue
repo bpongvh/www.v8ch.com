feb6f77080473de3f681ea3050285888095b4a06
Robert Pratt <bpong@v8ch.com>
2018-09-02 17:35:31 +0700

    Integrate AppContext, Offcanvas, Topbar

--- priv/src/js/components/App.vue
+++ priv/src/js/components/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <home />
+  <div>
+    <offcanvas @toggle-offcanvas="toggleOffcanvas" :isOffcanvasShowing="isOffcanvasShowing" />
+    <router-view />
+  </div>
 </template>
 
 <script type="text/babel">
+import AppContext from "../app-context";
 import Home from './home/Home.vue';
+import Offcanvas from './Offcanvas.vue';
 
 export default {
-  components: { Home },
+  components: { Home, Offcanvas },
+  data() {
+    return { isOffcanvasShowing: false };
+  },
+  methods: {
+    toggleOffcanvas() {
+      this.isOffcanvasShowing = !this.isOffcanvasShowing;
+    }
+  },  
+  mounted() {
+    AppContext.$on("toggle-offcanvas", () => {
+      this.toggleOffcanvas();
+    });
+  },
+  watch: {
+    "$route"() {
+      this.isOffcanvasShowing = false;
+    }
+  }
 };
 </script>
