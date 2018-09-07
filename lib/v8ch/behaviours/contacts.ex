b3a0601c7fd8fdbd162ca363c2eb2fcee737986e
Robert Pratt <bpong@v8ch.com>
2018-09-07 19:50:04 +0700

    Add get_contact

--- lib/v8ch/behaviours/contacts.ex
+++ lib/v8ch/behaviours/contacts.ex
@@ -2,4 +2,5 @@ defmodule V8ch.Behaviours.Contacts do
   alias V8ch.Contacts.Contact
 
   @callback create_contact(arg :: %{email: String.t(), message: String.t()}) :: {:ok, [Contact.t()]}
+  @callback get_contact(arg :: Integer.t()) :: {:ok, [Contact.t()]}
 end
