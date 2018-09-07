9e0606a02154d49c89411fc7d62b53d7174dd078
Robert Pratt <bpong@v8ch.com>
2018-09-07 19:49:39 +0700

    Removed

--- lib/v8ch/behaviours/sessions.ex
+++
@@ -1,4 +0,0 @@
-defmodule V8ch.Behaviours.Sessions do
-
-  @callback create_session(arg :: %{code: String.t()}) :: {:ok}
-end
--- lib/v8ch_web/graphql/resolvers/session.ex
+++
@@ -1,9 +0,0 @@
-defmodule V8chWeb.GraphQlResolvers.Session do
-  @session_module Application.get_env(:v8ch, :session_module)
-
-  def create_session(_, %{data: params}, _) do
-    with {:ok, session} <- @session_module.create_session(params) do
-      {:ok, %{session: session}}
-    end
-  end
-end
