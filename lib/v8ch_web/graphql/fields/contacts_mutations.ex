db174b033b8bc0551cc280cb6bcfd42b2b41e378
Robert Pratt <bpong@v8ch.com>
2018-09-07 19:51:25 +0700

    Initial commit

---
+++ lib/v8ch_web/graphql/fields/contacts_mutations.ex
@@ -0,0 +1,30 @@
+defmodule V8chWeb.GraphQlFields.ContactsMutations do
+  use Absinthe.Schema.Notation
+
+  alias V8chWeb.GraphQlMiddleware.CheckAuthentication, as: CheckAuthenticationMiddleware
+  alias V8chWeb.GraphQlMiddleware.HandleChangesetErrors, as: HandleChangesetErrorsMiddleware
+  alias V8chWeb.GraphQlResolvers.Contacts, as: ContactsResolver
+
+  object :contacts_mutations do
+    field :create_contact, :contact_mutation_result do
+      arg(:data, non_null(:contact_create_input))
+      middleware CheckAuthenticationMiddleware
+      resolve(&ContactsResolver.create_contact/3)
+      middleware HandleChangesetErrorsMiddleware
+    end
+
+    field :delete_contact, :contact_mutation_result do
+      arg(:id, non_null(:integer))
+      middleware CheckAuthenticationMiddleware
+      resolve(&ContactsResolver.delete_contact/3)
+      middleware HandleChangesetErrorsMiddleware
+    end
+
+    field :update_contact, :contact_mutation_result do
+      arg(:data, non_null(:contact_update_input))
+      middleware CheckAuthenticationMiddleware
+      resolve(&ContactsResolver.update_contact/3)
+      middleware HandleChangesetErrorsMiddleware
+    end
+  end
+end
---
+++ lib/v8ch_web/graphql/types/contact.ex
@@ -0,0 +1,22 @@
+defmodule V8chWeb.GraphQlTypes.Contact do
+  use Absinthe.Schema.Notation
+
+  @desc "Contact query object"
+  object :contact do
+    field(:email, :string)
+    field(:message, :string)
+    field(:inserted_at, :iso8601)
+  end
+
+  @desc "Contact mutation result object"
+  object :contact_mutation_result do
+    field :contact, :contact
+    field :errors, list_of(:input_error)
+  end
+
+  @desc "Contact input object for create requests"
+  input_object :contact_create_input do
+    field :email, non_null(:string)
+    field :message, non_null(:string)
+  end
+end
