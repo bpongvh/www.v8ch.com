f593874f8442361250c7954933e7835a1c70c19f
Robert Pratt <bpong@v8ch.com>
2018-09-08 14:22:27 +0700

    Remove deprecated import

--- priv/src/js/components/App.vue
+++ priv/src/js/components/App.vue
@@ -8,11 +8,10 @@
 <script type="text/babel">
 import {mapMutations, mapState} from "vuex";
 import { types as sessionMutations } from "../store/session/mutations";
-import Home from './home/Home.vue';
 import Offcanvas from './Offcanvas.vue';
 
 export default {
-  components: { Home, Offcanvas },
+  components: { Offcanvas },
   computed: {...mapState("session", {isOffcanvasShowing: (state) => state.isOffcanvasShowing})},
   methods: { ...mapMutations("session", {toggleOffcanvas: sessionMutations.TOGGLE_OFFCANVAS}) }, 
   watch: {
