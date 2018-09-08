239aaa03e8fe0987dd703ca9e20bfae6527b157a
Robert Pratt <bpong@v8ch.com>
2018-09-08 14:22:03 +0700

    Authentication checks

--- priv/src/js/components/Admin.vue
+++ priv/src/js/components/Admin.vue
@@ -1,32 +1,37 @@
 <template>
   <div>
     <offcanvas @toggle-offcanvas="toggleOffcanvas" :isOffcanvasShowing="isOffcanvasShowing" />
-    <router-view />
+    <router-view v-if="isAuthenticated" />
   </div>
 </template>
 
 <script type="text/babel">
   import { mapActions, mapMutations, mapState } from "vuex";
   import { types as sessionMutations } from "../store/session/mutations";
-  import Dashboard from './dashboard/Dashboard.vue';
   import Offcanvas from './Offcanvas.vue';
 
   export default {
-    components: { Dashboard, Offcanvas },
+    components: { Offcanvas },
     computed: {
       ...mapState("session", {
         errors: state => state.errors,
-        isOffcanvasShowing: state => state.isOffcanvasShowing,
-        tokens: state => state.tokens }
+        isAuthenticated: state => !!state.tokens,
+        isOffcanvasShowing: state => state.isOffcanvasShowing
+      }
     )},
     methods: {
       ...mapActions("session", ["fetchTokens"]),
       ...mapMutations("session", {toggleOffcanvas: sessionMutations.TOGGLE_OFFCANVAS})
     },  
     mounted() {
+      // Authentication checks
       // Handles case of redirect to /dashboard on login
-      if (this.$route.query.code & !this.tokens) {
+      if (this.$route.query.code && !this.isAuthenticated) {
         this.fetchTokens(this.$route.query.code);
+      }
+      // Handles case of redirect direct navigation and not authenticated
+      if (!this.$route.query.code && !this.isAuthenticated) {
+        window.location.href = `${window.location.origin}/login`;
       }
     },
     watch: {
