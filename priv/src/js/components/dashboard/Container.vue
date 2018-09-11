d9b208fa7c1ed43a94d4a292bb06bf6ee98b5db9
Robert Pratt <bpong@v8ch.com>
2018-09-11 07:13:46 +0700

    Add contact list

--- priv/src/js/components/dashboard/Container.vue
+++ priv/src/js/components/dashboard/Container.vue
@@ -1,15 +1,27 @@
 <template>
     <div>
+      <topbar @toggle-offcanvas="toggleOffcanvas" theme="dark" />
+      <header class="section-header">
+        <h2>Dashboard</h2>
+      </header>
+      <div class="container">        
         <contact-list :contacts="contacts" />
+      </div>
     </div>
 </template>
 
 <script type="text/babel">
   import gql from 'graphql-tag';
+  import {mapMutations} from "vuex";
+  import { types as sessionMutations } from "../../store/session/mutations";
   import ContactList from "../contact/List";
+  import Topbar from "../shared/Topbar";
 
   export default {
     apollo: { contacts: gql`{contacts {email id insertedAt message}}` },
-    components: {ContactList}
+    components: {ContactList, Topbar},
+    methods: {
+      ...mapMutations("session", {toggleOffcanvas: sessionMutations.TOGGLE_OFFCANVAS})
+    },
   }
 </script>
