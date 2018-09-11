f4bb79741876aa5748bbaf87ee1690a35db7dbc2
Robert Pratt <bpong@v8ch.com>
2018-09-11 08:49:03 +0700

    Refactor for contact-list-item, add recent posts

--- priv/src/js/components/dashboard/Container.vue
+++ priv/src/js/components/dashboard/Container.vue
@@ -5,7 +5,12 @@
         <h2>Dashboard</h2>
       </header>
       <div class="container">        
-        <contact-list :contacts="contacts" />
+        <content-list title="Contacts">
+          <contact-list-item :contact="contact" :key="index" v-for="(contact, index) in contacts" />
+        </content-list>       
+        <content-list title="Recent Posts">
+          <post-list-item :post="post" :key="index" v-for="(post, index) in posts" />
+        </content-list>
       </div>
     </div>
 </template>
@@ -14,12 +19,17 @@
   import gql from 'graphql-tag';
   import {mapMutations} from "vuex";
   import { types as sessionMutations } from "../../store/session/mutations";
-  import ContactList from "../contact/List";
+  import ContactListItem from "../contact/ListItem";
+  import PostListItem from "../post/ListItem";
+  import ContentList from "../shared/ContentList";
   import Topbar from "../shared/Topbar";
 
   export default {
-    apollo: { contacts: gql`{contacts {email id insertedAt message}}` },
-    components: {ContactList, Topbar},
+    apollo: {
+      contacts: gql`{contacts {email id insertedAt message}}`,
+      posts: gql`{posts {content id insertedAt title}}`
+    },
+    components: {ContactListItem, ContentList, PostListItem, Topbar},
     methods: {
       ...mapMutations("session", {toggleOffcanvas: sessionMutations.TOGGLE_OFFCANVAS})
     },
