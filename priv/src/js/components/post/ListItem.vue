6b82cf4b47db52a7ab15dcb2fc2522e2b24ebc3e
Robert Pratt <bpong@v8ch.com>
2018-09-11 12:50:27 +0700

    Add edit buttong

--- priv/src/js/components/post/ListItem.vue
+++ priv/src/js/components/post/ListItem.vue
@@ -5,6 +5,7 @@
       <p>{{posted}}</p>
     </div>
     <div class="list-group-item__actions">
+      <button class="btn btn-link" type="button">Edit</button>
       <button class="btn btn-link" type="button">Delete</button>
     </div>
   </div>
