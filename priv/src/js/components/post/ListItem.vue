920e785ff1602787df81ae88dd75a071281bec99
Robert Pratt <bpong@v8ch.com>
2018-09-11 08:48:11 +0700

    Initial commit

---
+++ priv/src/js/components/post/ListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="list-group-item">
+    <div class="list-group-item__header">
+      <h4>{{post.title}}</h4>
+      <p>{{posted}}</p>
+    </div>
+    <div class="list-group-item__actions">
+      <button class="btn btn-link" type="button">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script type="text/babel">
+  import moment from "moment";
+  import Vue from "vue";
+
+  export default {
+    computed: {
+      posted() {
+        return moment(this.post.insertedAt).fromNow();
+      }
+    },
+    props: {
+      post: {
+        required: true,
+        type: Object
+      }
+    }
+  }
+</script>
