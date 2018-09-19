65ce1d85cf1e575c479f828baefe89c1a09f784a
Robert Pratt <bpong@v8ch.com>
2018-09-19 08:36:22 -0500

    Update for renaming

--- priv/src/js/transformers/response-data/post.js
+++ priv/src/js/transformers/response-data/post.js
@@ -1,9 +1,9 @@
 import DOMPurify from "dompurify";
 import moment from "moment";
-import markdown from "../markdown";
+import markdownTransformer from "../markdown";
 
 export default function(postResponseJson) {
-  const html = markdown.makeHtml(postResponseJson.content);
+  const html = markdownTransformer.makeHtml(postResponseJson.content);
   return {
     content: DOMPurify.sanitize(html),
     published: moment().format("ll"),
