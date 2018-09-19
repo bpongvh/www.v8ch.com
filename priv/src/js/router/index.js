9f38244d9a32b80a66e0b50cb230f1cb2f2c632b
Robert Pratt <bpong@v8ch.com>
2018-09-19 08:36:05 -0500

    Add posts, postAdd routes

--- priv/src/js/router/index.js
+++ priv/src/js/router/index.js
@@ -1,6 +1,8 @@
 import BlogContainer from "../components/blog/Container";
 import DashboardContainer from "../components/dashboard/Container";
 import HomeContainer from "../components/home/Container";
+import PostAdd from "../components/post/Add";
+import PostContainer from "../components/post/Container";
 
 const routes = [
   {
@@ -17,6 +19,16 @@ const routes = [
     component: DashboardContainer,
     name: "dashboard",
     path: "/dashboard"
+  },
+  {
+    component: PostContainer,
+    name: "posts",
+    path: "/post"
+  },
+  {
+    component: PostAdd,
+    name: "postAdd",
+    path: "/post/add"
   }
 ];
 
