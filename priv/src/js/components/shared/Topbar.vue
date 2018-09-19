0f28141ffa384647f6084a4b42c55884b205a5d7
Robert Pratt <bpong@v8ch.com>
2018-09-19 08:35:05 -0500

    Refactor

--- priv/src/js/components/shared/Topbar.vue
+++ priv/src/js/components/shared/Topbar.vue
@@ -1,6 +1,9 @@
 <template>
   <nav :class="{'navbar-dark': isDark}" class="fixed-top navbar">
-      <button @click="toggleOffcanvas" class="navbar-toggler">
+      <button @click="closeOverlay" class="navbar-toggler" v-if="isOverlay">
+        <font-awesome-icon icon="times" />
+      </button>
+      <button @click="toggleOffcanvas" class="navbar-toggler" v-else>
         <font-awesome-icon icon="bars" />
       </button>
   </nav>
@@ -11,7 +14,22 @@ import {mapMutations} from "vuex";
 import { types as sessionMutations } from "../../store/session/mutations";
 
 export default {
+  computed: {
+    isOverlay() {
+      return this.type === "overlay"
+    }
+  },
   methods: { ...mapMutations("session", {toggleOffcanvas: sessionMutations.TOGGLE_OFFCANVAS}) },
-  props: { isDark: Boolean }
+  props: {
+    closeOverlay: {
+      required: false,
+      type: Function
+    },
+    isDark: Boolean,
+    type: {
+      required: false,
+      type: String
+    }
+  }
 };
 </script>
